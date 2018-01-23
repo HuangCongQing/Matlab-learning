2ebbe95c6aad28548394809d2dc42113859dc118
HuangChongqing <1756260160@qq.com>
2018-01-23 16:04:34 +0800

    定积分

--- 08_Algrithms/integral.m
+++ 08_Algrithms/integral.m
@@ -13,3 +13,16 @@ int(sym(f))
 syms a t
 int(a*cos(pi*t))
 int(a^x)
+
+% 定积分
+int(x, 4, 9)
+% 
+% 下面来计算x轴和曲线y = x^3-2x + 5和纵坐标x = 1和x = 2之间的面积。
+% 
+% 所需面积由公式计算 -
+
+f = x^3 - 2*x +5;
+a = int(f, 1, 2)
+display('Area: '), disp(double(a));
+
+
