c2f1c91fab91087bd3f8431887e92b3381c42577
HuangChongqing <1756260160@qq.com>
2018-01-23 15:07:59 +0800

    绘图属性

--- 05_Figure/figure51.m
+++ 05_Figure/figure51.m
@@ -1,8 +1,8 @@
 % http://www.51zxw.net/show.aspx?id=33623&cid=456
-x = 0:0.1:2*pi; % 步长0.1就行
+x = 0:0.5:2*pi; % 步长0.01就行
 y1 = sin(x);
 
-plot(x, y1);
+% plot(x, y1);
 xlabel('X轴');
 ylabel('y轴');
 
@@ -16,11 +16,15 @@ axis([0 2*pi -1 1]);
 % 若要显示另一个图，输入hold on
 hold on;%%%%
 y2 = cos(x);
-plot(x,y2);
+% plot(x,y2);
 
 % 另一种显示方法
 
-plot(x, y1, x, y2);
+plot(x, y1,'-.g', x, y2,'--or');
 
 % 添加说明，一个图就显示一个
 legend('这是说明1-正弦函数','说明2-余弦曲线');
+
+%%%% 绘图属性  线性，线宽，颜色
+
+
